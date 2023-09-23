3b88ce1347ec84a4ee1d464a8f704f9db2d8fab4
Varun Agrawal <varagrawal@gmail.com>
2023-09-23 16:02:39 -0400

    small fixes

--- python/nah/loader.py
+++ python/nah/loader.py
@@ -12,7 +12,7 @@ def get_filename(participant_id,
                  gesture_num,
                  demo_num,
                  follow_up=False):
-    """Get the experiment data filename based on arguments."""
+    """Get the experiment data CSV filename based on arguments."""
 
     # Path to project root directory
     project_root = Path(__file__).parent.parent.parent.resolve()
@@ -25,7 +25,8 @@ def get_filename(participant_id,
         is_followup = ''
 
     participant_directory = f"PID{participant_id}{is_followup}"
-    experiment_file = f"{robot_name}_PID{participant_id}{is_followup}_{end_eff_name}" f"Motion_gesture_{gesture_num}_{demo_num}.csv"
+    experiment_file = f"{robot_name}_PID{participant_id}{is_followup}_{end_eff_name}"\
+        f"Motion_gesture_{gesture_num}_{demo_num}.csv"
 
     filename = data_path / participant_directory / experiment_file
 
@@ -55,14 +56,14 @@ def load_alternate_data(robot_name, gesture_num, demo_num, followup=True):
     The start time should be identical, though, and the overall time as well
 
     (I prefer to have the code do this for me,
-    rather than keeping track of which PIDs I had to adjust manually)
+    rather than keeping track of which participant IDs I had to adjust manually)
     """
     if followup:
-        PID_temp = 9
+        participant_id = 9
     else:
-        PID_temp = 3
+        participant_id = 3
 
-    filename = get_filename(participant_id=PID_temp,
+    filename = get_filename(participant_id=participant_id,
                             robot_name=robot_name,
                             end_eff_name="Joint",
                             follow_up=followup,
@@ -72,17 +73,16 @@ def load_alternate_data(robot_name, gesture_num, demo_num, followup=True):
         jointangles = pd.read_csv(filename)
         joint_data = jointangles.to_numpy()
 
-        print(f"Using PID {PID_temp} instead")
+        print(f"Using participant ID {participant_id} instead")
         return joint_data
 
-    except FileNotFoundError:
-        raise FileNotFoundError(
-            f"JointMotion file not found AND backup JointMotion file not found: {filename}"
-        )
+    except FileNotFoundError as exc:
+        error_msg = f"JointMotion file not found AND backup JointMotion file not found: {filename}"
+        raise FileNotFoundError(error_msg) from exc
 
 
-def dtw_data_import(robot_name, end_eff_name, PID, followup, gesture_num,
-                    demo_num):
+def dtw_data_import(robot_name, end_eff_name, participant_id, followup,
+                    gesture_num, demo_num):
     """
     Import data for running DTW.
 
@@ -94,11 +94,11 @@ def dtw_data_import(robot_name, end_eff_name, PID, followup, gesture_num,
     # Y-position of hand and end-effector tend to be most consistently aligned.
     # Use those to discover the time warp mapping
 
-    filename = get_filename(PID, robot_name, end_eff_name, gesture_num,
-                            demo_num, followup)
+    filename = get_filename(participant_id, robot_name, end_eff_name,
+                            gesture_num, demo_num, followup)
 
     try:
-        # Import data from csvs
+        # Load data from CSV
         controller_raw = pd.read_csv(filename)
         controller_data = controller_raw.to_numpy()
         return controller_data
--- python/tests/test_trajectory_alignment.py
+++ python/tests/test_trajectory_alignment.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import numpy as np
 import pytest
-from nah.alignments import dtw_data_import, get_filename
+from nah.loader import dtw_data_import, get_filename
 
 
 @pytest.fixture
@@ -14,4 +14,3 @@ def trajectory():
     print(traj_file)
     traj = np.load(traj_file)
     return traj
-
