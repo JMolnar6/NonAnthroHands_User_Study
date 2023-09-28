88a1f5fb1999b621011e5deeea4bcb0224bbf3fd
Jennifer Molnar <jmolnar@gatech.edu>
2023-09-27 21:18:46 -0400

    handling type errors in trajectory.py + adding optional auto-plotting for debug purposes

--- python/nah/trajectory.py
+++ python/nah/trajectory.py
@@ -3,7 +3,7 @@
 import numpy as np
 from evo.core import metrics, sync
 from evo.core.trajectory import PoseTrajectory3D
-from nah.alignments import Alignment, dtw_align, evo_align, manifold_align
+from nah.alignments import Alignment, dtw_align, evo_align, manifold_align, pose_dist
 from scipy.spatial.transform import Rotation
 
 
@@ -48,7 +48,7 @@ def evaluate_ape(traj1: PoseTrajectory3D, traj2: PoseTrajectory3D):
     return metric
 
 
-def get_evo_metrics(traj1, traj2, alignment=Alignment.No):
+def get_evo_metrics(traj1, traj2, alignment=Alignment.No, suppress_plots=True):
     """
     Take two trajectories of equal length and calculate
     the error between them.
@@ -69,33 +69,44 @@ def get_evo_metrics(traj1, traj2, alignment=Alignment.No):
         # traj2_evo = manifold_align(traj1_evo, traj2_evo)
 
     elif alignment == Alignment.Temporal:
-        # traj2_evo = dtw_align(traj2_evo_spatial, traj1_evo)
-        raise NotImplementedError("Temporal alignment not implemented.")
+        traj1_evo = convert_evo_to_np(traj1_evo)
+        traj2_evo = convert_evo_to_np(traj2_evo)
+        traj1_aligned, traj2_aligned = dtw_align(traj1_evo, traj2_evo, dist=pose_dist)
+        traj1_evo = get_evo_trajectory(traj1_aligned)
+        traj2_evo = get_evo_trajectory(traj2_aligned)
+
+        # raise NotImplementedError("Temporal alignment not implemented.")
 
     elif alignment == Alignment.SpatioTemporal:
-        traj2_evo_spatial = manifold_align(traj1_evo, traj2_evo)
+        #TODO(Jennifer) Varun, I wanted to do spatiotemporal alignment and 
+        # right now, evo_alignment is working better. I commented out the
+        # manifold_alignment but it's ready here for you to re-insert. The 
+        # Alignment description in nah.alignments matches what's here
+        # traj2_evo_spatial = manifold_align(traj1_evo, traj2_evo)
+        traj2_evo_spatial = evo_align(traj1_evo, traj2_evo)
         traj1_spatial = convert_evo_to_np(traj1_evo)
         traj2_spatial = convert_evo_to_np(traj2_evo_spatial)
-        traj1_aligned, traj2_aligned = dtw_align(traj1_spatial, traj2_spatial)
+        traj1_aligned, traj2_aligned = dtw_align(traj1_spatial, traj2_spatial, dist=pose_dist)
         traj1_evo = get_evo_trajectory(traj1_aligned)
         traj2_evo = get_evo_trajectory(traj2_aligned)
 
     else:
         raise RuntimeError("Invalid Alignment specified.")
 
-    # # The following is just for debug. Comment it out when you're actually running
-    # # the correlation matrix code; this is for making sure the DTW stuff is working
-    # from nah.plot import plot_pos, plot_rot
-    # try:
-    #     traj1_np = convert_evo_to_np(traj1_evo)
-    #     traj2_np = convert_evo_to_np(traj2_evo)
-    #     plot_pos(traj1_np[:, 1:4], traj2_np[:, 1:4], traj1_np[:, 0],
-    #              traj2_np[:, 0])
-    #     plot_rot(traj1_np[:, 4:7], traj2_np[:, 4:7], traj1_np[:, 0],
-    #              traj2_np[:, 0])
-    # except:
-    #     print("Plot data failed")
-    #     raise
+    if not suppress_plots:
+        # The following is just for debug. Comment it out when you're actually running
+        # the correlation matrix code; this is for making sure the DTW stuff is working
+        from nah.plot import plot_pos, plot_rot
+        try:
+            traj1_np = convert_evo_to_np(traj1_evo)
+            traj2_np = convert_evo_to_np(traj2_evo)
+            plot_pos(traj1_np[:, 1:4], traj2_np[:, 1:4], traj1_np[:, 0],
+                    traj2_np[:, 0])
+            plot_rot(traj1_np[:, 4:7], traj2_np[:, 4:7], traj1_np[:, 0],
+                    traj2_np[:, 0])
+        except:
+            print("Plot data failed")
+            raise
 
     metric = evaluate_ape(traj1_evo, traj2_evo)
     return metric.get_all_statistics()
